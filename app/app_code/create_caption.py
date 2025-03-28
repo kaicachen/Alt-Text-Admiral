61f11275df98efee5776d8645fa78378459993c0
john-d-newman <123586142+JohnDNewman@users.noreply.github.com>
2025-03-28 14:00:55 -0500

    Added support to collect training data

---
+++ app/app_code/create_caption.py
@@ -0,0 +1,88 @@
+import os
+from image_processing import *
+from text_processing import *
+from sentence_generator import *
+
+def mergeTags(entities):  # Function to merge tags
+    
+    for category in entities:  # Iterate through all tags to check for words starting with "##"
+        fixedTags = []  # Create an empty array to fix all of the tags for the category, resets for each category
+        temp_name = ""  # Stores the tag that we are merging, will get added to fixedTags when merged
+
+        for name in entities[category]:
+            if name.startswith("##"):  # If "##" is at the start, we need to combine it with the previous name
+                temp_name += name[2:]  # Grab the slice of everything after '##' and append it to the previous tag
+            else:
+                if temp_name:  # The temp name is the whole word and no more merging needs to be done
+                    fixedTags.append(temp_name)  # Save the previous merged name
+                temp_name = name  # Move on to the next name
+
+        if temp_name:  # Add the last processed name, this happens if the last word needs to be merged
+            fixedTags.append(temp_name)
+
+        entities[category] = fixedTags  # Makes the category the new fixed tags
+    return entities  # Returns the entire entities dictionary
+
+
+def create_caption(image_path, text, URL=False, training=True):
+    URL = image_path.startswith("http") or image_path.startswith("https")
+    image_processor = ImageProcessor(image_path, URL=URL)  # Instantiate an Image Processor Class
+
+    caption = image_processor.generate_caption_with_blip()  # Generate caption through Salesforce Blip captioning
+    detected_objects = image_processor.find_image_objects()  # Extract tags from image (image_processing.py)
+
+    # Skip if no information is extracted from the image, likely due to an error
+    if caption == "" and detected_objects == {}:
+        return ""
+
+    entities = extract_entities(text)   # Extracts tags from text (text_processing.py)
+    entities = mergeTags(entities)  # Fixes issue where some words would be prepended by "##"
+    # Example: [Leb, ##ron James]  -> [Lebron James]
+
+    tags = ""  # Create tags variable to store all gathered tags
+
+    for object, quantity in detected_objects.items():  # Add image tags to tag string
+        # cur_string = f"{object} {quantity}, "
+        cur_string = f"{object}, "
+        tags += cur_string
+    
+    # Add text tags to tag string
+    for person in entities["People"]:  # Add all people
+        tags += f"{person}, "
+    for person in entities["Organizations"]: # Add all organizations
+        tags += f"{person}, "
+
+    # We just do this for now because the OTHER category usually isn't important for alt text
+
+    # for tag, tag_list in entities.items():
+    #     cur_string = ""
+    #     for entity in tag_list:
+    #         cur_string += f"{entity}, "
+    #     tags += cur_string
+
+    # for tag, tag_list in entities.items():
+    #     cur_string = f"{tag}: ("
+    #     for entity in tag_list:
+    #         cur_string += f"{entity}, "
+    #     cur_string += ") "
+    #     tags += cur_string
+
+    print(f"Caption: {caption}\nTags: {tags}")
+    if training:
+        return train_generate_sentence(caption, tags, image_path)
+    else:
+        return generate_sentence(caption, tags)  # Pass the created caption and extracted tags to our alt-text generator
+
+if __name__ == "__main__":
+    # image_path = "images/basketball.jpg"
+    # text = "Elon Musk is the CEO of Tesla and SpaceX. Steve Jobs was the co-founder of Apple and ate an apple every day."
+
+    '''
+    Source for the following image and text are https://www.espn.com/nba/story/_/id/43833377/a-rivalry-bromance-failed-reunion-steph-kd-lebron-reunite-all-star-weekend
+    '''
+
+    image_path = "basketball.jpg"
+    text = "No. 17 Kansas defeated Colorado 71-59 on Tuesday night at Allen Fieldhouse. The Jayhawks (17-7, 8-5 Big 12) won their first of two matchups between the sides. A big reason for that was KU’s defense — a calling card for Bill Self teams. The Jayhawks stepped up on that end in pivotal moments, doing so in a new look of sorts on Tuesday."
+
+    caption = create_caption(os.path.join("app", "app_code", "inputs", "Images", image_path), text)
+    print(f"Caption: {caption}")
---
+++ app/app_code/main_captioner.py
@@ -0,0 +1,109 @@
+import os
+import re
+from create_caption import *
+from web_scraper import *
+from csv_to_pdf import *
+import multiprocessing
+import csv
+import time
+
+# Takes in split list of images and text from main function and returns them captioned with run time
+def _multiprocess_caption(site_data, process_number, return_values):
+    processed_images = []
+    start_time = time.time()
+    for image, text in site_data:
+        processed_images.append((
+            image,
+            create_caption(image, text, URL=True)
+        ))
+    end_time = time.time()
+
+    return_values[process_number] = (processed_images, end_time - start_time)
+
+# Captions entire site with no multiprocessing by defaults
+def caption_site(url, output_name='site', pool=1):
+    site_data = scrape(url)
+
+    if site_data is None:
+        return
+
+    if output_name == 'site':
+        output_name = re.sub(r'[\/:*?"<>|]', '-', url)[:20]
+
+    # No multiprocessing
+    if pool == 1:
+        with open(os.path.join("app", "app_code", "outputs", "CSVs", f"{output_name}_pool_{pool}.csv"), mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            
+            # Write a header row (optional)
+            writer.writerow(["image_link", "generated_output"])
+            
+            for image, text in site_data:
+                writer.writerow([
+                    image,
+                    create_caption(image, text, URL=True, training=True)
+                ])
+
+    # Multiprocessing
+    else:
+        sublistSize  = len(site_data) // pool                 # number of images per multiprocessor list
+        splitSiteData = []                                              # creates list that will store subsets of the possible keys for multiprocessing
+        processList   = []                                              # creates list that will store the process references
+        return_values = multiprocessing.Manager().dict()                # initialized shared dictionary for return values
+        site_data_iterator = iter(site_data)
+
+        # startTime = time.perf_counter()                                 # records the start time of the iteration
+        for i in range(pool):                                           # iterates over the number of processes to split
+            splitSiteData.append([])                                         # appends a new list to store each process' site data
+            for j in range(sublistSize):                                    # iterates over the set size of the sublist
+                splitSiteData[i].append(next(site_data_iterator))                # appends the next value of the itertools iterator
+
+        for i in range(len(splitSiteData)):                              # iterates through the split key lists
+            processList.append(multiprocessing.Process(target=_multiprocess_caption, args=(splitSiteData[i], i, return_values)))
+                                                                            # appends process reference calling tryKeys with each possibleKey sublist
+            processList[i].start()                                          # starts the process
+
+        for process in processList:                                     # iterates through the processes
+            process.join()                                                  # joins them to end the processes
+
+        processList.clear()                                             # clears the process references
+
+        with open(os.path.join("app", "app_code", "outputs", "CSVs", f"{output_name}_pool_{pool}.csv"), mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            
+            # Write a header row (optional)
+            writer.writerow(["image_link", "generated_output"])
+        
+            captioned_images = []
+            run_times = []
+
+            for i in return_values:
+                captioned_images.extend(return_values[i][0])
+                run_times.append(return_values[i][1])
+
+            for image, text in captioned_images:
+                writer.writerow([
+                    image,
+                    text
+                ])
+
+            writer.writerow(["RUN_TIMES", run_times])
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    
+    output_name = "ku_lied"
+    site_url = "https://lied.ku.edu"
+    # caption_site(site_url, output_name=output_name)
+    # create_pdf(f"{output_name}_pool_1")
+    
+    # end_time = time.time()
+    # print(f"Execution time: {end_time - start_time:.2f} seconds")
+
+    start_time = time.time()
+    pool = 4
+    caption_site(site_url, output_name=output_name, pool=pool)
+    create_pdf(f"{output_name}_pool_{pool}")
+    end_time = time.time()
+    print(f"Execution time: {end_time - start_time:.2f} seconds")
