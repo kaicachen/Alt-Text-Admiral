ba506c50047d9bf2fc47cd5bc546097c773c3fc4
Aiden Patel <aiden.patel21@gmail.com>
2025-04-17 21:16:42 -0500

    Change import

--- app/index.py
+++ app/index.py
@@ -17,7 +17,7 @@ from flask_session import Session
 from os import environ, urandom
 from os import name as os_name
 from dotenv import load_dotenv
-import main
+import app.main as main
 
 
 # Load environmental variables
